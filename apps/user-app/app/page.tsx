c8911446977ba546ddd5124d7e24a627b6414602
Wadad Parker <wadadparker5@gmail.com>
2024-03-23 21:49:20 +0530

    feat: Added Recoil
    The import error for recoil is still pending

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,15 @@
+'use client'
 import Image from "next/image";
 
+import { useBalance } from "@repo/store/balance";
+
 export default function Page(): JSX.Element {
+
+  const balance = useBalance();
+
   return (
     <main className="w-screen h-screen">
-      <h1 className=" text-3xl font-medium">Hello Za Worldooo</h1>
+      <h1 className=" text-3xl font-medium">Hello Za Worldooo {balance}</h1>
       </main>
   );
 }
